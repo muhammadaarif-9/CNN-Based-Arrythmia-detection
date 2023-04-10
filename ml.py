1df56b152b7993c3d534aab2ed1e1773ee612c04
Muhammad Aarif <105774383+muhammadaarif-9@users.noreply.github.com>
2023-04-10 15:37:29 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,21 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # this is used for the plot the graph 
+import seaborn as sns # used for plot interactive graph.
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import average_precision_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve
+from sklearn.metrics import f1_score
+from sklearn.metrics import auc
+from sklearn.svm import SVC
